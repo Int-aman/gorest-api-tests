957452f06eda7d74e7e88f2e82939c479b4c4f28
Int-aman <aman.yadav3@swiggy.in>
2022-08-02 15:11:58 +0530

    Initial Commit

---
+++ app/src/test/java/com/ultralesson/gorestapitests/SampleTests.java
@@ -0,0 +1,11 @@
+package com.ultralesson.gorestapitests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTests {
+    @Test
+    public void testSomething(){
+        Assert.assertEquals(1,1);
+    }
+}
